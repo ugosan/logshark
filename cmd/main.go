cc934485a7ff65c203a2a6a543367d4d8faccc26
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2020-11-23 10:15:46 -0500

    s p a c e s

--- cmd/main.go
+++ cmd/main.go
@@ -2,115 +2,115 @@
 package main
 
 import (
-	"github.com/ugosan/logshark/cmd/server"
-	"github.com/rivo/tview"
-	"time"
-	"fmt"
-	"encoding/json"
-	"github.com/TylerBrock/colorjson"
+  "github.com/ugosan/logshark/cmd/server"
+  "github.com/rivo/tview"
+  "time"
+  "fmt"
+  "encoding/json"
+  "github.com/TylerBrock/colorjson"
 )
 
 var menu = tview.NewList()
 var c = make(chan map[string]interface{})
 var app = tview.NewApplication()
 func read_events(){
-	//log.Printf("epa")
-	f := colorjson.NewFormatter()
-	f.Indent = 4
-		
-	for {
-		obj := <-c
-		
+  //log.Printf("epa")
+  f := colorjson.NewFormatter()
+  f.Indent = 4
+    
+  for {
+    obj := <-c
+    
     //s, _ := f.Marshal(obj)
-		//fmt.Println(string(s))
-		//j, err := json.MarshalIndent(obj, "", "")
+    //fmt.Println(string(s))
+    //j, err := json.MarshalIndent(obj, "", "")
 
-		s, _ := json.Marshal(obj)
-		
-		menu.AddItem(string(s), "", 0, nil)
-		
-		
-		//if err != nil {
-		//	log.Printf("epa")
-		//}
-	}
+    s, _ := json.Marshal(obj)
+    
+    menu.AddItem(string(s), "", 0, nil)
+    
+    
+    //if err != nil {
+    //	log.Printf("epa")
+    //}
+  }
 
 
 }
 
 //instead of redrawing at every event, redraws every 300 microseconds
 func refresh() {
-	for {
-		app.Draw()
-		time.Sleep(200 * time.Microsecond) 
-	}
+  for {
+    app.Draw()
+    time.Sleep(200 * time.Microsecond) 
+  }
 }
 
 func main() {
 
-	go server.Start(c)
-	go read_events()
-	go refresh()
+  go server.Start(c)
+  go read_events()
+  go refresh()
 
 
-	menu.
-	AddItem("0 List item 1", "", 0, nil).
-	AddItem("1 List item 2", "", 0, nil)
+  menu.
+  AddItem("0 List item 1", "", 0, nil).
+  AddItem("1 List item 2", "", 0, nil)
 
-	for i := 1; i <= 100; i++ {
-		
-		menu.AddItem(fmt.Sprintf("List item %d", i), "", 0, nil)
-	}
+  for i := 1; i <= 100; i++ {
+    
+    menu.AddItem(fmt.Sprintf("List item %d", i), "", 0, nil)
+  }
 
-	menu.ShowSecondaryText(false)
+  menu.ShowSecondaryText(false)
 
-	main := tview.NewTextView().
-		SetDynamicColors(true).
-		SetRegions(true).
-		SetWordWrap(true).
-		SetChangedFunc(func() {
-			app.Draw()
-		})
+  main := tview.NewTextView().
+    SetDynamicColors(true).
+    SetRegions(true).
+    SetWordWrap(true).
+    SetChangedFunc(func() {
+      app.Draw()
+    })
 
-		
+    
 
-	var obj map[string]interface{}
-	json.Unmarshal([]byte("{ \"hola\": \"hola\"}"), &obj)
+  var obj map[string]interface{}
+  json.Unmarshal([]byte("{ \"hola\": \"hola\"}"), &obj)
 
-	// Make a custom formatter with indent set
-	f := colorjson.NewFormatter()
-	f.Indent = 4
+  // Make a custom formatter with indent set
+  f := colorjson.NewFormatter()
+  f.Indent = 4
 
-	// Marshall the Colorized JSON
-	s, _ := f.Marshal(obj)
+  // Marshall the Colorized JSON
+  s, _ := f.Marshal(obj)
 
-	fmt.Fprintf(main, "%s", tview.TranslateANSI(string(s)))
+  fmt.Fprintf(main, "%s", tview.TranslateANSI(string(s)))
 
-	menu.SetBorder(true)
-	main.SetBorder(true)
+  menu.SetBorder(true)
+  main.SetBorder(true)
 
-	title :=  tview.NewTextView().
-	SetDynamicColors(true).
-	SetText(" Logshark ")
+  title :=  tview.NewTextView().
+  SetDynamicColors(true).
+  SetText(" Logshark ")
 
-	footer :=  tview.NewTextView().
-	SetDynamicColors(true).
-	SetText(" [blue]R[white]efresh [blue]S[white]ettings ").
-	SetChangedFunc(func() {
-		app.Draw()
-	})
+  footer :=  tview.NewTextView().
+  SetDynamicColors(true).
+  SetText(" [blue]R[white]efresh [blue]S[white]ettings ").
+  SetChangedFunc(func() {
+    app.Draw()
+  })
 
-	layout := tview.NewFlex().
-	AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(title, 1, 1, false).
-		AddItem(menu, 0, 1, false).
-		AddItem(main, 0, 3, false).
-		AddItem(footer, 1, 1, false), 0, 2, false)
+  layout := tview.NewFlex().
+  AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+    AddItem(title, 1, 1, false).
+    AddItem(menu, 0, 1, false).
+    AddItem(main, 0, 3, false).
+    AddItem(footer, 1, 1, false), 0, 2, false)
 
 
-	if err := app.SetRoot(layout, true).SetFocus(menu).EnableMouse(true).Run(); err != nil {
-		panic(err)
-	}
+  if err := app.SetRoot(layout, true).SetFocus(menu).EnableMouse(true).Run(); err != nil {
+    panic(err)
+  }
 
 
 
