07f72d68f47df833bf9336cd635e7c9d2c1bbc8d
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2020-12-29 08:43:29 -0500

    formatting

--- v1/ui/ui.go
+++ v1/ui/ui.go
@@ -1,183 +1,179 @@
 package ui
 
-
 import (
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/TylerBrock/colorjson"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+	"github.com/ugosan/logshark/v1/config"
+	"github.com/ugosan/logshark/v1/logging"
 	"github.com/ugosan/logshark/v1/server"
-  "github.com/ugosan/logshark/v1/config"
-  "github.com/ugosan/logshark/v1/logging"
-  logshark_widgets "github.com/ugosan/logshark/v1/widgets"
-  "github.com/TylerBrock/colorjson"
-  ui "github.com/gizak/termui/v3"
-  "github.com/gizak/termui/v3/widgets"
-  "fmt"
-  "time"
-  "regexp"
-  "encoding/json"
+	logshark_widgets "github.com/ugosan/logshark/v1/widgets"
 )
-  
+
 var (
-  channel = make(chan map[string]interface{})
-  events []string
-  redrawFlag = true
-  eventList = widgets.NewList()
-  eventView = logshark_widgets.NewParagraph()
-  footer = logshark_widgets.NewFooter()
-  grid = ui.NewGrid()
-  logs = logging.GetManager()
-  
-  
+	channel    = make(chan map[string]interface{})
+	events     []string
+	redrawFlag = true
+	eventList  = widgets.NewList()
+	eventView  = logshark_widgets.NewParagraph()
+	footer     = logshark_widgets.NewFooter()
+	grid       = ui.NewGrid()
+	logs       = logging.GetManager()
 
-  keysRegex, _ = regexp.Compile(`(\[37m)(.*?)(\[0m)`)
-  stringsRegex, _ = regexp.Compile(`(\[32m)(.*?)(\[0m)`)
-  numbersRegex, _ = regexp.Compile(`(\[36m)(.*?)(\[0m)`)
-  booleanRegex, _ = regexp.Compile(`(\[33m)(.*?)(\[0m)`)
-  termWidth = 0
-  termHeight = 0
-  formatter = colorjson.NewFormatter()
+	keysRegex, _    = regexp.Compile(`(\[37m)(.*?)(\[0m)`)
+	stringsRegex, _ = regexp.Compile(`(\[32m)(.*?)(\[0m)`)
+	numbersRegex, _ = regexp.Compile(`(\[36m)(.*?)(\[0m)`)
+	booleanRegex, _ = regexp.Compile(`(\[33m)(.*?)(\[0m)`)
+	termWidth       = 0
+	termHeight      = 0
+	formatter       = colorjson.NewFormatter()
 )
 
+func readEvents() {
 
-func readEvents(){
+	for {
+		obj := <-channel
 
-  for {
-    obj := <-channel
+		prettyJson, _ := formatter.Marshal(obj)
+		events = append(events, translateANSI(string(prettyJson)))
 
-    prettyJson, _ := formatter.Marshal(obj)
-    events = append(events, translateANSI(string(prettyJson)))
-    
-    s, _ := json.Marshal(obj)
-    eventList.Rows = append(eventList.Rows, fmt.Sprintf("%d %s", len(events), string(s)))
+		s, _ := json.Marshal(obj)
+		eventList.Rows = append(eventList.Rows, fmt.Sprintf("%d %s", len(events), string(s)))
 
-    redrawFlag = true
-  }
+		redrawFlag = true
+	}
 
 }
 
 //instead of redrawing at every event, redraws every 300 microseconds
 func redraw() {
-  if(redrawFlag){
-      
-    ui.Render(eventView, eventList, footer)
-    redrawFlag = false
-  }
+	if redrawFlag {
+
+		ui.Render(eventView, eventList, footer)
+		redrawFlag = false
+	}
 }
 
 func reset() {
-  events = events[:0]
-  eventList.Rows = []string{}
-  eventView.Text = ""
-  server.ResetStats()
+	events = events[:0]
+	eventList.Rows = []string{}
+	eventView.Text = ""
+	server.ResetStats()
 }
 
 func translateANSI(s string) string {
 
-  s = stringsRegex.ReplaceAllString(s, "<$2>(fg:yellow)")
-  s = numbersRegex.ReplaceAllString(s, "<$2>(fg:magenta)")
-  s = keysRegex.ReplaceAllString(s, "<$2>(fg:blue)")
-  s = booleanRegex.ReplaceAllString(s, "<$2>(fg:green)")
+	s = stringsRegex.ReplaceAllString(s, "<$2>(fg:yellow)")
+	s = numbersRegex.ReplaceAllString(s, "<$2>(fg:magenta)")
+	s = keysRegex.ReplaceAllString(s, "<$2>(fg:blue)")
+	s = booleanRegex.ReplaceAllString(s, "<$2>(fg:green)")
 
-  return s
+	return s
 }
 
 func updateEventView() {
 
-  if(eventList.SelectedRow>-1){
+	if eventList.SelectedRow > -1 {
 
-    eventView.Text = events[eventList.SelectedRow]
+		eventView.Text = events[eventList.SelectedRow]
 
-    ui.Render(eventList, eventView)
-  }
+		ui.Render(eventList, eventView)
+	}
 
 }
 
 func updateStats() {
 
-  stats := fmt.Sprintf(" %d/%d | %d e/s ", server.GetStats().Events, server.GetStats().MaxEvents, server.GetStats().Eps)
+	stats := fmt.Sprintf(" %d/%d | %d e/s ", server.GetStats().Events, server.GetStats().MaxEvents, server.GetStats().Eps)
 
-  footer.Text = stats
-  ui.Render(footer)
+	footer.Text = stats
+	ui.Render(footer)
 }
-
 
 func Start(config config.Config) {
 
-  go server.Start(channel, config)
+	go server.Start(channel, config)
 
-  if err := ui.Init(); err != nil {
-    logs.Log(err)
-  }
-  defer ui.Close()
+	if err := ui.Init(); err != nil {
+		logs.Log(err)
+	}
+	defer ui.Close()
 
-  formatter.Indent = 2
-  eventView.Title = "preview"
-  footer.Border = false
-  footer.WrapText = false
-  footer.TextStyle.Fg = ui.ColorBlack
-  footer.TextStyle.Bg = ui.ColorWhite
+	formatter.Indent = 2
+	eventView.Title = "preview"
+	footer.Border = false
+	footer.WrapText = false
+	footer.TextStyle.Fg = ui.ColorBlack
+	footer.TextStyle.Bg = ui.ColorWhite
 
-  eventList.Title = "List"
-  eventList.TextStyle = ui.NewStyle(ui.ColorYellow)
-  eventList.WrapText = false
+	eventList.Title = "List"
+	eventList.TextStyle = ui.NewStyle(ui.ColorYellow)
+	eventList.WrapText = false
 
-  grid := ui.NewGrid()
-  termWidth, termHeight = ui.TerminalDimensions()
-  
-  grid.SetRect(0, 0, termWidth, termHeight-1)
+	grid := ui.NewGrid()
+	termWidth, termHeight = ui.TerminalDimensions()
 
-  grid.Set(
-    ui.NewRow(1,
-      ui.NewCol(0.2, eventList),
-      ui.NewCol(0.8, eventView),
-    ),
-  )
+	grid.SetRect(0, 0, termWidth, termHeight-1)
 
-  footer.SetRect(0, termHeight-1, termWidth, termHeight)
+	grid.Set(
+		ui.NewRow(1,
+			ui.NewCol(0.2, eventList),
+			ui.NewCol(0.8, eventView),
+		),
+	)
 
-  ui.Render(grid)
-  
-  go readEvents()
+	footer.SetRect(0, termHeight-1, termWidth, termHeight)
 
-  uiEvents := ui.PollEvents()
-  ticker := time.NewTicker(time.Microsecond*300).C
+	ui.Render(grid)
 
-  for {
-    select {
-    case e := <-uiEvents:
-      switch e.ID {
-      case "q", "<C-c>":
-        return
-      case "<Down>":
-        eventList.ScrollDown()
-        updateEventView()
-      case "<Up>":
-        eventList.ScrollUp()
-        updateEventView()
-      case "r":
-        reset()
-        ui.Render(grid)
-      case "t":
-        server.SendTestRequest()
-        ui.Render(grid)
-      case "<Resize>":
-        payload := e.Payload.(ui.Resize)
+	go readEvents()
 
-        termWidth = payload.Width
-        termHeight = payload.Height
-        
-        grid.SetRect(0, 0, termWidth, termHeight-1)
-        footer.SetRect(0, payload.Height-1, payload.Width, payload.Height)
-        ui.Clear()
-        ui.Render(grid)
-      }
-    case <-ticker:
-      updateStats()
-      if(len(eventList.Rows) == 1){
-        eventList.SelectedRow = 0
-        updateEventView()
-      }
-      
-      redraw()
-    }
-  }
+	uiEvents := ui.PollEvents()
+	ticker := time.NewTicker(time.Microsecond * 300).C
+
+	for {
+		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			case "<Down>":
+				eventList.ScrollDown()
+				updateEventView()
+			case "<Up>":
+				eventList.ScrollUp()
+				updateEventView()
+			case "r":
+				reset()
+				ui.Render(grid)
+			case "t":
+				server.SendTestRequest()
+				ui.Render(grid)
+			case "<Resize>":
+				payload := e.Payload.(ui.Resize)
+
+				termWidth = payload.Width
+				termHeight = payload.Height
+
+				grid.SetRect(0, 0, termWidth, termHeight-1)
+				footer.SetRect(0, payload.Height-1, payload.Width, payload.Height)
+				ui.Clear()
+				ui.Render(grid)
+			}
+		case <-ticker:
+			updateStats()
+			if len(eventList.Rows) == 1 {
+				eventList.SelectedRow = 0
+				updateEventView()
+			}
+
+			redraw()
+		}
+	}
 
 }
